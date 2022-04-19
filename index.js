895aa126869583304db3e29846898a4005d0c28d
Harsha Vamsi <harshavamsi096@gmail.com>
2022-04-19 13:18:19 -0700

    updates

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const filters = {
   ],
   'Time Poverty Status': [
     { text: 'Time Poor', id: 'time_poor', value: '1.0' },
-    { text: 'Not Time Poor', id: 'time_poor', value: '1.0' },
+    { text: 'Not Time Poor', id: 'time_poor', value: '0.0' },
   ],
   'Main Mode of Transportation': [
     { text: 'Car User', id: 'car_user', value: '1.0' },
@@ -287,6 +287,8 @@ document.addEventListener('DOMContentLoaded', function () {
         plotUnWeighted(i);
       });
     }
+
+    plotHeatMap(data);
   });
 });
 
@@ -538,4 +540,69 @@ function downloadDataSet(key) {
     const data = [...group.get(true).values()];
     exportFromJSON({ data, fileName, fields: [], exportType: 'csv' });
   }
+}
+
+function unpack(rows, key) {
+  return rows.map(function (row) {
+    return row[key];
+  });
+}
+
+function plotHeatMap(data) {
+  var data = [
+    {
+      type: 'choropleth',
+
+      locationmode: 'USA-states',
+
+      locations: unpack(data, 'state'),
+
+      z: unpack(data, 'norm_wb'),
+
+      text: unpack(data, 'state'),
+
+      zmin: 0,
+
+      zmax: 17000,
+
+      colorscale: [
+        [0, 'rgb(242,240,247)'],
+        [0.2, 'rgb(218,218,235)'],
+
+        [0.4, 'rgb(188,189,220)'],
+        [0.6, 'rgb(158,154,200)'],
+
+        [0.8, 'rgb(117,107,177)'],
+        [1, 'rgb(84,39,143)'],
+      ],
+
+      colorbar: {
+        title: 'Millions USD',
+
+        thickness: 0.2,
+      },
+
+      marker: {
+        line: {
+          color: 'rgb(255,255,255)',
+
+          width: 2,
+        },
+      },
+    },
+  ];
+
+  var layout = {
+    title: '2011 US Agriculture Exports by State',
+
+    geo: {
+      scope: 'usa',
+
+      showlakes: true,
+
+      lakecolor: 'rgb(255,255,255)',
+    },
+  };
+
+  Plotly.newPlot('heatmap', data, layout, { showLink: false });
 }
